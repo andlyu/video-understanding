a7f8e95c19770d930f3441b4434ebc649f38a387
Andrew Lyubovsky <a.l.andlyu@gmail.com>
2023-11-18 17:54:50 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+**Ego Schema Zero-shot Video Question Answering**
+  
+Setting up kaggle
+  
+First, copy kaggle key from local env
+Second, run command to download echoschmea
+```
+cat ~/.kaggle/kaggle.json
+touch  ~/.kaggle/kaggle.json
+
+pip install kaggle
+kaggle competitions download -c egoschema-public
+```
